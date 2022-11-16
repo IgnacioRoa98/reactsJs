3cc42bcb154cfacf2c3a768dfa52b2a5cbbb6243
Ignacio Roa <ignacioroa98@gmail.com>
2022-11-16 19:31:02 -0300

    cambios

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1 className='h1'> Esto es un Navbar</h1>
+            <button className="boton"> inicio</button>
+            <button> Productos </button>
+        </nav>
+    )
+}
+export default Navbar
